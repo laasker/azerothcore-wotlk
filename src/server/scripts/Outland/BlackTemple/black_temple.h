1ae8e61bd75f2dc0f5414cbae75af93ab11b6554
laasker <79991768+laasker@users.noreply.github.com>
2025-08-08 00:19:28 -0300

    (Core/BlackTemple.h) Fix Dementia BT

--- src/server/scripts/Outland/BlackTemple/black_temple.h
+++ src/server/scripts/Outland/BlackTemple/black_temple.h
@@ -110,8 +110,8 @@ enum MiscIds
     SPELL_WYVERN_STING              = 24336,
     SPELL_SHADOW_INFERNO_DAMAGE     = 39646,
     SPELL_CHAOTIC_CHARGE            = 41033,
-    SPELL_DEMENTIA1                 = 41406,
-    SPELL_DEMENTIA2                 = 41409,
+    SPELL_DEMENTIA1                 = 41409, //
+    SPELL_DEMENTIA2                 = 41409, //
     SPELL_SUMMON_SHADOWFIENDS       = 41159
 };
 

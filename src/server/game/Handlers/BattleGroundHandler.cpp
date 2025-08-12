5a96632694f8bd5ddc26039ed2258c391b662a04
laasker <79991768+laasker@users.noreply.github.com>
2025-08-12 00:46:12 -0300

    [PvP] (BattleGroundHandler.cpp) Pode sair de BG/Arena em combat agora

--- src/server/game/Handlers/BattleGroundHandler.cpp
+++ src/server/game/Handlers/BattleGroundHandler.cpp
@@ -598,10 +598,10 @@ void WorldSession::HandleBattlefieldLeaveOpcode(WorldPacket& recvData)
     recvData.read_skip<uint16>();                          // unk3
 
     // not allow leave battleground in combat
-    if (_player->IsInCombat())
-        if (Battleground* bg = _player->GetBattleground())
-            if (bg->GetStatus() != STATUS_WAIT_LEAVE)
-                return;
+    //if (_player->IsInCombat())
+    //    if (Battleground* bg = _player->GetBattleground())
+    //        if (bg->GetStatus() != STATUS_WAIT_LEAVE)
+    //            return;
 
     _player->LeaveBattleground();
 }

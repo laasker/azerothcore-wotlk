2e2fbfe2e6c26044c9d651daaac9600d19bc1d3b
laasker <79991768+laasker@users.noreply.github.com>
2025-08-12 11:33:56 -0300

    [Custom] (SpellEffects.cpp) Ritual of Summoning vs Mall

--- src/server/game/Spells/SpellEffects.cpp
+++ src/server/game/Spells/SpellEffects.cpp
@@ -1071,6 +1071,13 @@ void Spell::EffectTriggerRitualOfSummoning(SpellEffIndex effIndex)
     if (effectHandleMode != SPELL_EFFECT_HANDLE_HIT)
         return;
 
+    // do not allow summoning in bgs and donor malls
+    //if (m_caster->ToPlayer()->InBattleground()) /* || m_caster->GetMapId() == 13 || m_caster->GetMapId() == 598)*/
+    //{
+    //    SendCastResult(SPELL_FAILED_NOT_HERE);
+    //    return;
+    //}
+
     uint32 triggered_spell_id = m_spellInfo->Effects[effIndex].TriggerSpell;
     SpellInfo const* spellInfo = sSpellMgr->GetSpellInfo(triggered_spell_id);
 
@@ -5410,6 +5417,13 @@ void Spell::EffectTransmitted(SpellEffIndex effIndex)
 {
     if (effectHandleMode != SPELL_EFFECT_HANDLE_HIT)
         return;
+
+    /*// do not allow summoning in bgs and donor malls
+    if (m_caster->GetMapId() == 13 || m_caster->GetMapId() == 598)
+    {
+        SendCastResult(SPELL_FAILED_NOT_HERE);
+        return;
+    }*/
 
     uint32 name_id = m_spellInfo->Effects[effIndex].MiscValue;
 

d7641c5f097dd3bf7db92abd66006684c91c8022
laasker <79991768+laasker@users.noreply.github.com>
2025-08-12 12:20:57 -0300

    [PvP] (player.cpp) testar - afk in arena remove de BG ?

--- src/server/game/Entities/Player/Player.cpp
+++ src/server/game/Entities/Player/Player.cpp
@@ -1301,7 +1301,7 @@ void Player::ToggleAFK()
     ToggleFlag(PLAYER_FLAGS, PLAYER_FLAGS_AFK);
 
     // afk player not allowed in battleground
-    if (!IsGameMaster() && isAFK() && InBattleground())
+    if (!IsGameMaster() && isAFK() && InBattleground() &&InArena())
         LeaveBattleground();
 }
 

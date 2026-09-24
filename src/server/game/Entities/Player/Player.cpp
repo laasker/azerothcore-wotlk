3010f8ad3fcede2f206c1a5d1472bbc281880062
laasker <reksalx@gmail.com>
2026-09-24 18:23:18 -0300

    (Player.cpp) Duel Distance (50/40 > 80/70 yards)

--- src/server/game/Entities/Player/Player.cpp
+++ src/server/game/Entities/Player/Player.cpp
@@ -6595,7 +6595,7 @@ void Player::CheckDuelDistance(time_t currTime)
 
     if (!duel->OutOfBoundsTime)
     {
-        if (!IsWithinDistInMap(obj, 50))
+        if (!IsWithinDistInMap(obj, /*50*/ 80))
         {
             duel->OutOfBoundsTime = currTime + 10;
 
@@ -6605,7 +6605,7 @@ void Player::CheckDuelDistance(time_t currTime)
     }
     else
     {
-        if (IsWithinDistInMap(obj, 40))
+        if (IsWithinDistInMap(obj, /*40*/ 70))
         {
             duel->OutOfBoundsTime = 0;
 

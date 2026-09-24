c5b4e377dd0ba503cc294103febc397821332b86
laasker <reksalx@gmail.com>
2026-09-24 08:09:05 -0300

    Revert "(Unit.cpp/cs_reload/RBAC) Add SpellRegulator"
    This reverts commit 263df6388a20c3d8f14680a5829127aaf22c45f4.

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -76,7 +76,6 @@
 #include <algorithm>
 #include <cmath>
 #include <limits>
-#include "../../../modules/mod-spell-regulator/src/SpellRegulator.h"
 
 float baseMoveSpeed[MAX_MOVE_TYPE] =
 {
@@ -1006,11 +1005,6 @@ uint32 Unit::DealDamage(Unit* attacker, Unit* victim, uint32 damage, CleanDamage
 
     // Hook for OnDamage Event
     sScriptMgr->OnDamage(attacker, victim, damage);
-
-    if ((damagetype == SPELL_DIRECT_DAMAGE || damagetype == DOT) && spellProto)
-    {
-        sSpellRegulator->Regulate(damage, spellProto->Id);
-    }
 
     // Signal to pets that their owner was attacked - except when DOT.
     if (attacker != victim && damagetype != DOT)
@@ -6464,8 +6458,6 @@ void Unit::SendSpellNonMeleeReflectLog(SpellNonMeleeDamage* log, Unit* attacker)
 
 void Unit::SendSpellNonMeleeDamageLog(SpellNonMeleeDamage* log)
 {
-    sSpellRegulator->Regulate(log->damage, log->spellInfo->Id);
-
     WorldPacket data(SMSG_SPELLNONMELEEDAMAGELOG, (16 + 4 + 4 + 4 + 1 + 4 + 4 + 1 + 1 + 4 + 4 + 1)); // we guess size
     //IF we are in cheat mode we swap absorb with damage and set damage to 0, this way we can still debug damage but our hp bar will not drop
     uint32 damage = log->damage;
@@ -6568,7 +6560,6 @@ void Unit::ProcSkillsAndAuras(Unit* actor, Unit* victim, uint32 procAttacker, ui
 void Unit::SendPeriodicAuraLog(SpellPeriodicAuraLogInfo* pInfo)
 {
     AuraEffect const* aura = pInfo->auraEff;
-    sSpellRegulator->Regulate(pInfo->damage, aura->GetId());
     WorldPacket data(SMSG_PERIODICAURALOG, 30);
     data << GetPackGUID();
     data << aura->GetCasterGUID().WriteAsPacked();
